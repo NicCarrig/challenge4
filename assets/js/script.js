53406f689283811db4d8bc3fe016d93c04ee42ad
Nic Carrig <cleamcarrig90@hotmail.com>
2022-04-21 15:54:53 -0700

    added startQuiz() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 var startBtnEl = document.querySelector("#start-btn");
+var scoreBtnEl = document.querySelector(".high-score-btn");
+var questionAreaEl = document.querySelector(".question");
+var answersAreaEl = document.querySelector(".answers-area");
 var timerEl = document.querySelector("#timer");
 var timer = 0;
 var questionCounter = 0;
@@ -7,10 +10,10 @@ var questions = [
         qNum: 1,
         question: "What is the correct notation for an array?",
         choice: {
-            a: "''",
-            b: "()",
-            c: "[]",
-            d: "<>"
+            a: "'  '",
+            b: "( )",
+            c: "[ ]",
+            d: "< >"
         },
         correct: "c"
     },
@@ -18,10 +21,10 @@ var questions = [
         qNum: 2,
         question: "What is the correct notation for an array?",
         choice: {
-            a: "''",
-            b: "()",
-            c: "[]",
-            d: "<>"
+            a: "'  '",
+            b: "( )",
+            c: "[ ]",
+            d: "< >"
         },
         correct: "c"
     }
@@ -31,7 +34,42 @@ var questions = [
 function startQuiz(){
     //should populate the screen with the first question and 4 buttons for multiple choice answers
     //also needs to clear the start button and start the timer
+    startBtnEl.remove();
+    var letter = ["a", "b", "c", "d"];
+    //change text content from quiz rules to first question
+    questionAreaEl.textContent = questions[0].question
+    //create 4 buttons and populate them with the text in the arary
+    for(i=0; i < 4; i++){
+        var choiceList = document.createElement("li");
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "answer-btn");
+        choiceBtn.setAttribute("data-choice", letter[i]);
+        choiceBtn.textContent = setButtonText(i+1);
+        choiceList.appendChild(choiceBtn);
+        answersAreaEl.appendChild(choiceList);
+    }
     startTimer();
+}
+
+function setButtonText(index){
+    var str = '';
+    switch(index){
+        case 1:
+            str = questions[questionCounter].choice.a;
+            break;
+        case 2:
+            str = questions[questionCounter].choice.b;
+            break;
+        case 3:
+            str = questions[questionCounter].choice.c;
+            break;
+        case 4:
+            str= questions[questionCounter].choice.d;
+            break;
+        default:
+            console.log("error in setting button text");
+    }
+    return str;
 }
 
 function checkAnswer(){
@@ -64,7 +102,13 @@ function startTimer(){
 }
 
 function scoreScreen(){
+    //prompt for name or initials and save to local storage
     console.log("display the high scores");
 }
 
+function resetQuiz(){
+
+}
+
 startBtnEl.addEventListener("click", startQuiz);
+scoreBtnEl.addEventListener("click", scoreScreen);
